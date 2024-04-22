3ede8de99de8cbc409f1cf32917356d0608dd79a
miguelangelblancoguerrero <miguelangelblgu@ufps.edu.co>
2024-04-22 11:50:17 -0500

    asadas

--- script.js
+++ script.js
@@ -1,21 +1,38 @@
-const form = document.forms['loginForm'];
+document.addEventListener('DOMContentLoaded', function() {
+    const loginButton = document.getElementById('loginButton');
 
-form.addEventListener('sumbit',function handleFormSubmit(event){
-event.preventDefault();
-alert("Hols");
-const email = form['email'].value;
-const password = form['password'].value;
+    loginButton.addEventListener('click', function() {
+        const codigoEstudiante = document.getElementById('codigoEstudiante').value;
+        const contraseña = document.getElementById('contraseña').value;
 
-loginUser(email,password);
-});
+        const data = {
+            codigoEstudiante: codigoEstudiante,
+            contraseña: contraseña
+        };
 
-function loginUser(email, password) {
-    firebase.auth().signInWithEmailAndPassword(email, password)
-    .then(function (user) {
-      console.log('Credenciales correctas, ¡bienvenido! ' + user);
-      localStorage.setItem('user', JSON.stringify(user));
-    })
-    .catch(function (error) {
-      console.log(error);
+        // Realizar la solicitud POST
+        fetch('https://24a0dac0-2579-4138-985c-bec2df4bdfcc-00-3unzo70c406dl.riker.replit.dev/login', {
+            method: 'POST',
+            body: JSON.stringify(data),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Error en la solicitud.');
+            }
+            return response.json();
+        })
+        .then(userData => {
+            // Almacenar los datos del usuario en localStorage
+            localStorage.setItem('usuario', JSON.stringify(userData));
+            // Redirigir a la página de inicio
+            window.location.href = 'homepage.html';
+        })
+        .catch(error => {
+            console.error('Error:', error);
+            // Aquí puedes mostrar un mensaje de error al usuario si la solicitud falla
+        });
     });
-  }
+});
